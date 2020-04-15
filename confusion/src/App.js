aed331eb5a85a8c89c779695bb71d7f0ab70d1e9
Praneeth021 <kadampallypraneeth987@gmail.com>
2020-04-15 13:28:53 +0530

    Components Part-1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
           <NavbarBrand href='#'>Restaurant Confusion</NavbarBrand>
          </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
